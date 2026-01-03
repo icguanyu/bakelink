f83ea41921cc7fe611b5d1c37d10e8bfd7e02669
icguanyu <icguanyu@users.noreply.github.com>
2026-01-03 13:24:43 +0000

    deploy: 3310822ce7b44146847c6993a31d9ad782a229e7

--- assets/index-CE8ne8af.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./index-DN5jxiuq.js";const r={};function o(a,t){return n(),c("div",null," Baker Index (Dashboard) ")}const _=e(r,[["render",o]]);export{_ as default};
---
+++ assets/order-BFGTWLg1.js
@@ -0,0 +1,1 @@
+import{_ as o,j as r,r as t,o as c}from"./index-DOMPc8ao.js";const n={};function s(_,a){const e=t("router-view");return c(),r(e)}const p=o(n,[["render",s]]);export{p as default};
