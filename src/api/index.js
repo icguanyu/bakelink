4543e1d26d2c676d3b48cc9dfbe002f3b536b01c
Nick <icguanyu@gmail.com>
2026-02-16 16:50:47 +0800

    feat(api): 更新 API 基礎 URL 以支援生產環境

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+const mode = import.meta.env.MODE;
 const token = localStorage.getItem("bakelink-token");
 
 let isShowAlert = false; // 避免顯示太多次 ElMessageBox
@@ -7,7 +7,10 @@ let isShowAlert = false; // 避免顯示太多次 ElMessageBox
 if (token) {
   axios.defaults.headers.common["Authorization"] = "Bearer " + token;
 }
-axios.defaults.baseURL = import.meta.env.VITE_APIURL;
+axios.defaults.baseURL =
+  mode === "development"
+    ? "http://localhost:3000"
+    : import.meta.env.VITE_APIURL;
 
 axios.interceptors.response.use(
   (response) => {
