2a6baf3f6f4add8d0d9e438436f08be786bd2610
icguanyu <icguanyu@users.noreply.github.com>
2026-02-28 07:02:40 +0000

    deploy: 3a6b536a05294030cb997ae70768afca38e7c279

--- assets/schedules-MqkOXpAZ.js
+++
@@ -1,1 +0,0 @@
-import{X as t}from"./index-D3uRDPsG.js";const u={Create:e=>t.post("/schedules",e),GetByDate:e=>t.get(`/schedules/${e}`),GetByMonth:e=>t.get(`/schedules/month/${e}`),Update:(e,s)=>t.put(`/schedules/${e}`,s),Delete:e=>t.delete(`/schedules/${e}`)};export{u as S};
