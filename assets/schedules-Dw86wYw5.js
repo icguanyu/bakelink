541cc762847c2efa6853fc72a1eee24e45973245
icguanyu <icguanyu@users.noreply.github.com>
2026-06-03 06:41:43 +0000

    deploy: b42298e3651bd1e80688a5d4d5a7e3ecad384f8f

---
+++ assets/schedules-Dw86wYw5.js
@@ -0,0 +1,1 @@
+import{M as t}from"./index-DFKlW2-7.js";const u={Create:e=>t.post("/schedules",e),GetByDate:e=>t.get(`/schedules/${e}`),GetByMonth:e=>t.get(`/schedules/month/${e}`),Update:(e,s)=>t.put(`/schedules/${e}`,s),Delete:e=>t.delete(`/schedules/${e}`)};export{u as S};
