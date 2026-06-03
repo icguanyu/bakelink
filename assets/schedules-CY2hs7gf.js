0e11e6f9eae03e7b22c575c4c72269ab1b507f6b
icguanyu <icguanyu@users.noreply.github.com>
2026-06-03 07:24:32 +0000

    deploy: ec4e74539819325f72767cd409b3adb16885a4c9

---
+++ assets/schedules-CY2hs7gf.js
@@ -0,0 +1,1 @@
+import{N as t}from"./index-ARRRy2e1.js";const u={Create:e=>t.post("/schedules",e),GetByDate:e=>t.get(`/schedules/${e}`),GetByMonth:e=>t.get(`/schedules/month/${e}`),Update:(e,s)=>t.put(`/schedules/${e}`,s),Delete:e=>t.delete(`/schedules/${e}`)};export{u as S};
