825d3c4d27fc9683287b1d768284768f9fecdb5f
icguanyu <icguanyu@users.noreply.github.com>
2026-06-03 07:22:47 +0000

    deploy: 7ce864f9717d7af145d9688aabdef83ad81cc9de

--- assets/schedules-CPUDNxPu.js
+++
@@ -1,1 +0,0 @@
-import{M as t}from"./index-CaM3ErPw.js";const u={Create:e=>t.post("/schedules",e),GetByDate:e=>t.get(`/schedules/${e}`),GetByMonth:e=>t.get(`/schedules/month/${e}`),Update:(e,s)=>t.put(`/schedules/${e}`,s),Delete:e=>t.delete(`/schedules/${e}`)};export{u as S};
